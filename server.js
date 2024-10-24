aedb7a94ee0a9b751eff1d16b4a1a5682e0e6897
Nathan-Reis09 <nathan.hernandez.reis@gmail.com>
2024-10-24 05:33:09 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+
+import "dotenv/config";
+import app from "./src/app.js";
+const PORT = 3000;
+
+const rotas = {
+    "/": "Curso de API express",
+    "/livros": "entrei na rota livros",
+    "/": "entrei na rota autores"
+};
+
+app.listen(PORT, () => {
+    console.log("servidor escutando!");
+});
+
+
